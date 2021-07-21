0149dc2b425f59ca21f9f049a232d21856a14419
Corentin <corentin.briand@gmail.com>
2021-07-21 17:17:12 +0200

    add scroll snap order projects minimize nav on scroll

--- src/assets/js/styledNav.js
+++ src/assets/js/styledNav.js
@@ -1,7 +1,6 @@
 const nav = document.querySelector('nav');
 
 window.addEventListener('scroll', () => {
-  console.log('scrollpos', window.scrollY);
   if (scrollY > 10) {
     nav.classList.add('minimized');
   } else {
