f697823d5870878e29c2d1714efa6f8c78e3a622
Corentin <corentin.briand@gmail.com>
2021-07-30 19:12:07 +0200

    add mobile design for hero section

--- src/assets/js/styledNav.js
+++ src/assets/js/styledNav.js
@@ -1,9 +1,20 @@
-const nav = document.querySelector('nav');
+const nav = document.querySelector("nav");
+const navlinks = document.querySelector(".navlinks");
+const navToggle = document.querySelector("#navToggle");
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (scrollY > 10) {
-    nav.classList.add('minimized');
+    nav.classList.add("minimized");
   } else {
-    nav.classList.remove('minimized');
+    nav.classList.remove("minimized");
   }
 });
+
+navToggle.onclick = () => {
+  navlinks.classList.toggle("show");
+  if (navlinks.classList.contains("show")) {
+    navToggle.textContent = "Close";
+  } else {
+    navToggle.textContent = "Menu";
+  }
+};
